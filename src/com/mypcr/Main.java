7db5bdaa8c0e89e729e8fe463559b3050abadd3a
Park,Jung-Uk <dnr1105@gmail.com>
2015-12-31 20:39:39 +0900

    20151231_git, java basic mentoring

--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -2,14 +2,19 @@ package com.mypcr;
 
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import com.mypcr.emulator.MyPCR;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World 1");
-		System.out.println("Hello World 2");
-		System.out.println("Hello World 3");
-		System.out.println("Hello World 4");
-		System.out.println("Hello World 5");
-		System.out.println("Hello World 6");
+	public static void main(String[] args) throws AWTException {
+		int test = 1;
+
+		MyPCR mypcr = new MyPCR();
+
+		System.out.printf("%d + %d = %d\n", 1, 2, mypcr.add(1, 2));
+		System.out.printf("%d - %d = %d\n", 1, 2, mypcr.sub(1, 2));
+		System.out.printf("%d * %d = %d\n", 1, 2, mypcr.mul(1, 2));
+		System.out.printf("%d / %d = %d\n", 1, 2, mypcr.dev(1, 0));
 	}
 }
--- src/com/mypcr/emulator/MyPCR.java
+++ src/com/mypcr/emulator/MyPCR.java
@@ -2,4 +2,27 @@ package com.mypcr.emulator;
 
 public class MyPCR {
 
+	public MyPCR() {
+	}
+
+	public int add(int a, int b) {
+		return (a + b);
+	}
+
+	public int sub(int a, int b) {
+		return (a - b);
+	}
+
+	public int mul(int a, int b) {
+		return (a * b);
+	}
+
+	public int dev(int a, int b) {
+		if (b == 0) {
+			System.out.print("(Error, 분모가 0입니다.)");
+			return 0;
+		}
+		return (a / b);
+	}
+
 }
