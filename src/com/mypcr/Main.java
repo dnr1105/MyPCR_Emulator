4cfec16284c4a6639630fb5b9c4869d1571dd032
Park,Jung-Uk <dnr1105@gmail.com>
2016-01-21 21:04:09 +0900

    Week3 README.md file up

--- src/com/mypcr/Main.java
+++ src/com/mypcr/Main.java
@@ -1,5 +1,6 @@
 package com.mypcr;
 
+
 import java.awt.AWTException;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,29 +8,42 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import com.mypcr.emulator.Protocol;
 
-public class Main {
-	public static void main(String[] args) throws AWTException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = null;
-
-		ArrayList<Protocol> list = new ArrayList<Protocol>();
+public class Main
+{
+	public static void main( String[] args ) throws AWTException
+	{
+		BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
 		
-		Protocol p1 = new Protocol("1", 20, 20);
+		ArrayList< Protocol > list = new ArrayList< Protocol >( );
 		
-		list.add( p1 );
+		String pcr = "1	25	40\n" 
+					+"2	40	50\n" 
+					+"3	60	100\n";
+		String[] pcrs = pcr.split( "\n" );
 		
-		System.out.println(list.get(0).getLable( ));
-		System.out.println(list.get(0).getTemp( ));
-		System.out.println(list.get(0).getTime( ));
+		for( int i = 0; i < pcrs.length; i++ )
+		{
+			String[] temp = pcrs[i].split( "\t" );
+			
+			Protocol p = new Protocol(temp[0], Integer.parseInt( temp[1] ), Integer.parseInt( temp[2]));
+			list.add(p);
+		}
 		
-		/*for(String s : al){
-			System.out.println(s);
-		}*/
+		for(int i = 0; i < list.size( ); i++)
+		{
+			System.out.print( list.get( i ).getLable( ) + "\t");
+			System.out.print( list.get( i ).getTemp( )  + "\t");
+			System.out.print( list.get( i ).getTime( ) );
+			System.out.println(  );
+		}
 		
-		try {
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		try
+		{
+			br.close( );
+		}
+		catch( IOException e )
+		{
+			e.printStackTrace( );
 		}
 	}
 }
